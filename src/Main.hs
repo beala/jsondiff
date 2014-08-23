d22893fdb920c0f47ff64e235b25d9341e3e5b30
Alex Beal <alexlbeal@gmail.com>
2014-08-23 12:45:08 -0600

    Allow comparison to a node that doesn't exist by lifting nodes into a Maybe.

--- src/Main.hs
+++ src/Main.hs
@@ -12,9 +12,9 @@ main = do
   rawArgs <- getArgs
   let args = parseArgs rawArgs
   print $ do
-         t1 <- Atto.parseOnly json (fst args) 
-         t2 <- Atto.parseOnly json (snd args)
-         return $ diffJson t1 t2 [] []
+         t1 <- Atto.parseOnly value (fst args) 
+         t2 <- Atto.parseOnly value (snd args)
+         return $ diffMaybeJson (Just t1) (Just t2) [] []
 
 parseArgs :: [String] -> (B.ByteString, B.ByteString)
 parseArgs rawArgs = (B.pack (rawArgs !! 0), B.pack (rawArgs !! 1))
@@ -22,30 +22,39 @@ parseArgs rawArgs = (B.pack (rawArgs !! 0), B.pack (rawArgs !! 1))
 type Path = [String] 
 data Diff = Diff Path (Maybe Value) (Maybe Value) deriving Show
 
+diffMaybeJson :: Maybe Value -> Maybe Value -> Path -> [Diff] -> [Diff]
+diffMaybeJson (Just v1) (Just v2) p d = diffJson v1 v2 p d
+diffMaybeJson mV1 mV2 p d = (Diff p mV1 mV2) : d
+
 diffJson :: Value -> Value -> Path -> [Diff] -> [Diff]
 diffJson (Object o1) (Object o2) p d = undefined
 diffJson (Array a1) (Array a2) p d =
-    let matchedDiffs = (Vec.foldr diffElems d elemPairs) in 
-    diffUnmatchedArrayElems a1 a2 ("array" : p) matchedDiffs
-        where elemPairs = Vec.zip a1 a2
-              diffElems (aa1, aa2) dd = diffJson aa1 aa2 ("array" : p) dd
+    Vec.foldr diffElems d elemPairs
+        where elemPairs = zipAll (Vec.map Just a1) (Vec.map Just a2) Nothing Nothing
+              diffElems (aa1, aa2) dd = diffMaybeJson aa1 aa2 ("array" : p) dd
 diffJson s1@(String ss1) s2@(String ss2) p d = diffValues (s1, ss1) (s2, ss2) p d
 diffJson n1@(Number nn1) n2@(Number nn2) p d = diffValues (n1, nn1) (n2, nn2) p d
 diffJson b1@(Bool bb1) b2@(Bool bb2) p d     = diffValues (b1, bb1) (b2, bb2) p d
-diffJson Null Null p d                       = d
+diffJson Null Null _ d                       = d
 diffJson j1 j2 p d                           = (Diff p (Just j1) (Just j2)) : d
-
-diffUnmatchedArrayElems :: Vec.Vector Value -> Vec.Vector Value -> Path -> [Diff] -> [Diff]
-diffUnmatchedArrayElems a1 a2 p d
-    | Vec.length a1 > Vec.length a2 =
-        let unmatched = Vec.drop (Vec.length a2) a1 in
-        Vec.toList $ Vec.map (\a -> (Diff p (Just a) Nothing)) unmatched
-    | Vec.length a1 < Vec.length a2 =
-        let unmatched = Vec.drop (Vec.length a1) a2 in
-        Vec.toList $ Vec.map (\a -> (Diff p Nothing (Just a))) unmatched
-    | otherwise = []
 
 diffValues :: (Eq a) => (Value, a) -> (Value, a) -> Path -> [Diff] -> [Diff]
 diffValues (v1, vv1) (v2, vv2) p d
     | vv1 /= vv2  = (Diff p (Just v1) (Just v2)) : d
     | otherwise = d
+
+--printDiff :: Diff -> IO ()
+--printDiff (Diff p v1 v2) = putStrLn (reverse p) ++ " " ++ v1 ++ " " ++ v2
+
+zipAll :: Vec.Vector a -> Vec.Vector b -> a -> b -> Vec.Vector (a, b)
+zipAll v1 v2 pad1 pad2 = Vec.zip paddedV1 paddedV2
+    where padLen = max (Vec.length v1) (Vec.length v2)
+          paddedV1 = padTo v1 pad1 padLen
+          paddedV2 = padTo v2 pad2 padLen
+
+padTo :: Vec.Vector a -> a -> Int -> Vec.Vector a
+padTo v p l
+    | (Vec.length v) < l =
+        let paddingLen = l - Vec.length v in
+        v Vec.++ (Vec.replicate paddingLen p)
+    | otherwise = v
